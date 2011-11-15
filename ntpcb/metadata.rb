3b6897e22dbe444108cf7eee3d44975463ccdc13
Your Name <anandu@gmail.com>
2011-11-15 20:40:57 +0530

    testing attributes

--- ntpcb/metadata.rb
+++ ntpcb/metadata.rb
@@ -7,3 +7,10 @@ version          "0.0.1"
 recipe "ntpcb::ntp", "My first recipe, Installs/Configures ntp"
 recipe "ntpcb::cront", "Installs/Configures cRon"
 recipe "ntpcb::motds", "Welcomes the user"
+
+attribute "cront/tempdir",
+  :display_name => "Temp dir location",
+  :description => "The location of swap file.  Defaults to '/swapfile'.",
+  :type => "string",
+  :default => "/tmp",
+  :recipes => [ "ntpcb::cront"]
--- ntpcb/recipes/cront.rb
+++ ntpcb/recipes/cront.rb
@@ -1,10 +1,11 @@
+tempdir = node[:sys][:tempdir]
 cron "noop" do
   hour "1"
   minute "0"
   command "/sbin/ifconfig"
 end
 %w{dir1 dir2 dir3}.each do |dir|
-   directory "/tmp/mydirs/#{dir}" do
+   directory "#{tempdir}/#{dir}" do
       mode 0775
       owner "root"
       group "root"
