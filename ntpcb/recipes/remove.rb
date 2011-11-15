9328d730dfcb62759113487dd9f7ccc657aff713
Your Name <anandu@gmail.com>
2011-11-15 23:44:26 +0530

    remove dir1

--- ntpcb/recipes/remove.rb
+++ ntpcb/recipes/remove.rb
@@ -1,6 +1,6 @@
 tempdir = node[:ntpcb][:tempdir]
-%w{dir1 dir2 dir3}.each do |dir|
-directory "#{tempdir}/#{dir}" do
+#%w{dir1 dir2 dir3}.each do |dir|
+directory "#{tempdir}/dir1" do
   recursive true
   action :delete
 end
