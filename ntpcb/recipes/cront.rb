32c9144c01be641603ae817d3b90032c11cd4dc3
Your Name <anandu@gmail.com>
2011-11-15 18:32:02 +0530

    ressource dir

--- ntpcb/recipes/cront.rb
+++ ntpcb/recipes/cront.rb
@@ -3,3 +3,12 @@ cron "noop" do
   minute "0"
   command "/sbin/ifconfig"
 end
+%w{dir1 dir2 dir3}.each do |dir|
+   directory "/tmp/mydirs/#{dir}" do
+      mode 0775
+      owner "root"
+      group "root"
+      action :create
+      recursive true
+   end
+end
