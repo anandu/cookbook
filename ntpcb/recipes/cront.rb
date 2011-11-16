f4350448a93f43ecc1b71fc1751b1e39bc11eb50
Your Name <anandu@gmail.com>
2011-11-16 20:01:28 +0530

    Provider testing

--- ntpcb/recipes/cront.rb
+++ ntpcb/recipes/cront.rb
@@ -8,11 +8,7 @@ cron "noop" do
 end
 
 %w{dir1 dir2 dir3}.each do |dir|
-   directory "#{tempdir}/#{dir}" do
-      mode 0775
-      owner "root"
-      group "root"
-      action :create
-      recursive true
+   new_resource "#{tempdir}/#{dir}" do
+      action :mkd
    end
 end
