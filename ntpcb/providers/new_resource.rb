4174ccb4780a4b93857ccf37579d95706ad98f5b
Your Name <anandu@gmail.com>
2011-11-16 20:23:52 +0530

    Provider testing3

---
+++ ntpcb/providers/new_resource.rb
@@ -0,0 +1,11 @@
+action :mkd do
+  # Select recipe to make dir
+  # recipe = new_resource.cront
+  directory "#{tempdir}" do
+  mode 0775
+  owner "root"
+  group "root"
+  action :create
+  recursive true
+  end
+end
