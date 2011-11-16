502b3369fe4f16d04d8790079c55b279e9ea4e98
Your Name <anandu@gmail.com>
2011-11-16 20:12:21 +0530

    Provider testing2

--- ntpcb/recipes/cront.rb
+++ ntpcb/recipes/cront.rb
@@ -1,14 +1,15 @@
 tempdir = node[:ntpcb][:tempdir]
 
-cron "noop" do
-  hour "#{node[:ntpcb][:cron_backup_hr]}"
+#cron "noop" do
+#  hour "#{node[:ntpcb][:cron_backup_hr]}"
   #hour "1"
-  minute "0"
-  command "/sbin/ifconfig"
-end
+#  minute "0"
+#  command "/sbin/ifconfig"
+#end
 
-%w{dir1 dir2 dir3}.each do |dir|
-   new_resource "#{tempdir}/#{dir}" do
+#%w{dir1 dir2 dir3}.each do |dir|
+   #new_resource "#{tempdir}/#{dir}" do
+   new_resource "#{tempdir}" do
       action :mkd
    end
-end
+#end
